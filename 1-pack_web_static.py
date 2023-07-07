3fc7aafc436de07073e48f27b6e47046fec5b328
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-06 17:34:44 -0700

    Updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''Fabric script that generates a .tgz archive'''
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder
+"""
 
-from fabric.api import local
 from datetime import datetime
-from fabric.decorators import runs_once
+from fabric.api import local
 from os.path import isdir
 
 
-@runs_once
 def do_pack():
-    '''generates a .tgz archive from the contents of the web_static folder'''
+    """generates a .tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
