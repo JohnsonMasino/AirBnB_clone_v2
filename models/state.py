eec28fe700b16b6a86710be4ba76b8c50789ccaa
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-21 15:26:43 -0700

    Improve engines

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" holds class State"""
+""" holds class State and defined functions"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -11,7 +11,7 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
-    if models.storage_type == "db":
+    if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
@@ -22,7 +22,7 @@ class State(BaseModel, Base):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_type   != "db":
+    if models.storage_t != "db":
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
