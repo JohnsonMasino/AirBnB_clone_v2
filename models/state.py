d8bd67220e21db4d57f1f60050dadabf3139c84a
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-23 02:54:09 -0700

    Updated

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" holds class State and defined functions"""
+""" holds class State"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
